df5433e84cae85e52c6109a607399446db633a35
savagerunderscorelex <eigbekoyi@gmail.com>
2025-11-05 21:14:35 -0500

    Figured out how to compare string variables, added more tests, wrote more comments to explain code better

--- ShirleyJones.java
+++ ShirleyJones.java
@@ -21,6 +21,7 @@ public class ShirleyJones {
 
         Player userPlayer = new Player("Your Office", 100, 0);
         Clue[] cluesList = {firstClue, secondClue, thirdClue, fourthClue};
+
         // Actual Code
 
         // Scanners
@@ -31,34 +32,49 @@ public class ShirleyJones {
         System.out.println(cluesList[0].clueDesc); // Prints the first clue's description (makes sure that I can access the description through using the index of the clue + a method)
         InputFunctions.locationInputMethod(); // Creates a scanner, gets the input, and prints the input (makes sure that I wrote the function right and works as well as if I typed out all the code in the run function)
         
+
+        System.out.println(globalVariables.chosenLocation.getClass().getName()); // Prints the type of globalVariables.chosenLocation
+
+        
+        if (globalVariables.chosenLocation.equals("heyhey")){ // Testing if statements in java: checks if the globalVariables.chosenLocation variable is equal to the string "heyhey"
+            System.out.println("bye"); // if so, print this
+        } else {
+            System.out.println("you mdskjfdsk"); // if not print this
+        }
+
         InputFunctions.locationInputMethod(); // Second instance of the function to see if I can call this function again to make it work.
     }
 
 
     // Classes
 
+    public class globalVariables { // Global Variables Class: General Variables that I don't know where to put
+        static String chosenLocation; // Where the chosenLocation gets stored
+    }
+
     // Dialogues Class
 
     public class Dialogues {
-        static void playerDescription() {
+        static void playerDescription() { // Description of Shirley Jones
             System.out.println("Your name is Shirley Jones. You are a detective from Old Yap City. You are investigating a murder that occurred a couple of days ago.");
         }
+        static void printLocations() { // 7 Current Locations out of 11
+            System.out.println("Where do you want to go? Enter the number for the location you want to go to. 1) Your Office, 2) Martha's House, 3) David's Bakery, 4) Layla's Corner Store, 5) Officer Jason's Home, 6) Daria's Flower Shop, 7) Community Center: ");
+        }
 
-        static void printLocations() {
-            System.out.println("Where do you want to go? 1) Your Office, 2) Martha's House, 3) David's Bakery, 4) Layla's Corner Store, 5) Officer Jason's Home, 6) Daria's Flower Shop: ");
+        static void printNewClue() { // When the player gets a new clue, print this
+            System.out.println("You got a new clue! Go to your office to look over your list of clues.");
+        }
+    }
+
+    public class InputFunctions {
+        static void locationInputMethod() {
+            Scanner inputLocation = new Scanner(System.in);
+            globalVariables.chosenLocation = inputLocation.nextLine();
         }
     }
 
     // Player Class
-    public class InputFunctions {
-        static void locationInputMethod() {
-            Scanner inputLocation = new Scanner(System.in);
-            String chosenLocation = inputLocation.nextLine();
-            System.out.println(chosenLocation);
-        }
-    }
-
-
     static class Player {
         String location;
         int health;

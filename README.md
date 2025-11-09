51213765a799e41210a92718cc26dc96e5d63c8d
savagerunderscorelex <eigbekoyi@gmail.com>
2025-11-09 12:13:22 -0500

    updated readme with headings; created dialogue functions for location descriptions; added input validation for entering the game and for inputting a location number

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 ## About
 
 You play as Shirley Jones, a detective from Old Yap City who is investigating the murder of a beloved community member, Felicity Winslow. Your goal is to approach and interrogate the 9 suspects of her murder, study the available clues, and determine who murdered Felicity.
+
+## How to Play
+
+## Character Boards
--- ShirleyJones.java
+++ ShirleyJones.java
@@ -28,6 +28,7 @@ public class ShirleyJones {
         
 
         // Tests
+        InputFunctions.enterGame();
         System.out.println(userPlayer.health); // Prints Player Health (checks if I properly set up the player class)
         System.out.println(cluesList[0].clueDesc); // Prints the first clue's description (makes sure that I can access the description through using the index of the clue + a method)
         InputFunctions.locationInputMethod(); // Creates a scanner, gets the input, and stores it in the variable globalVariables.chosenLocation
@@ -40,6 +41,7 @@ public class ShirleyJones {
 
     public class globalVariables { // Global Variables Class: General Variables that I don't know where to put
         static String chosenLocation; // Where the chosenLocation gets stored
+        static String enterGameResponse;
     }
 
     // Dialogues Class
@@ -49,7 +51,7 @@ public class ShirleyJones {
             System.out.println("Your name is Shirley Jones. You are a detective from Old Yap City. You are investigating a murder that occurred a couple of days ago.");
         }
         static void printLocations() { // 7 Current Locations out of 11
-            System.out.println("Where do you want to go? Enter the number for the location you want to go to. 1) Your Office, 2) Martha's House, 3) David's Bakery, 4) Layla's Corner Store, 5) Officer Jason's Home, 6) Daria's Flower Shop, 7) Community Center: ");
+            System.out.print("Where do you want to go? Enter the number for the location you want to go to. 1) Your Office, 2) Martha's House, 3) David's Bakery, 4) Layla's Corner Store, 5) Officer Jason's Home, 6) Daria's Flower Shop, 7) Community Center: ");
         }
 
         static void printNewClue() { // When the player gets a new clue, print this
@@ -59,20 +61,68 @@ public class ShirleyJones {
         static void officeDescription() {
             System.out.println("You enter your office. Now you can review your list of gathered clues in peace.");
         }
+
+        static void marthaDescription() {
+
+        }
+        static void davidDescription() {
+
+        }
+
+        static void laylaDescription() {
+
+        }
+
+        static void jasonDescription() {
+
+        }
+
+        static void dariaDescription() {
+
+        }
+
+        static void emberDescription() {
+
+        }
     }
 
     public class InputFunctions {
         static void locationInputMethod() {
             Scanner inputLocation = new Scanner(System.in);
+            Dialogues.printLocations();
             globalVariables.chosenLocation = inputLocation.nextLine();
+            if (globalVariables.chosenLocation.equals("1")){
+                Dialogues.officeDescription();
+            } else if(globalVariables.chosenLocation.equals("2")) {
+                Dialogues.marthaDescription();
+            } else if(globalVariables.chosenLocation.equals("3")) {
+                Dialogues.davidDescription();
+            } else if(globalVariables.chosenLocation.equals("4")) {
+                Dialogues.laylaDescription();
+            } else if(globalVariables.chosenLocation.equals("5")) {
+                Dialogues.jasonDescription();
+            } else if(globalVariables.chosenLocation.equals("6")) {
+                Dialogues.dariaDescription();
+            } else if(globalVariables.chosenLocation.equals("7")) {
+                Dialogues.emberDescription();
+            }
+        }
+        static void enterGame() {
+            System.out.println("Welcome to Shirley Jones, a murder mystery game where you have to find out who killed the beloved star, Cimani. Press enter to play.");
+            Scanner enterKey = new Scanner(System.in);
+            globalVariables.enterGameResponse = enterKey.nextLine();
+            if (globalVariables.enterGameResponse.equals("")){
+                System.out.println("You have entered the game.");
+            } else {
+                System.out.println("Invalid input. Try again.");
+                InputFunctions.enterGame();
+            }
         }
     }
 
     public class validationFunctions{
         static void checkLocationInput(){
-            if (globalVariables.chosenLocation.equals("1")){
-                Dialogues.officeDescription();
-            }
+
         }
     }
     // Player Class
